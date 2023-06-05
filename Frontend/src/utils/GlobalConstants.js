771cead6dd7755a21b503e1b1bc8f1091c5d5324
Darshhhhh <contact.shahdarsh@gmail.com>
2023-06-05 17:48:03 +0530

    bug fix

--- Frontend/src/utils/GlobalConstants.js
+++ Frontend/src/utils/GlobalConstants.js
@@ -1,6 +1,6 @@
 export class GlobalConstants {
-  static domain = "http://localhost:5000/";
-  // static domain = "https://healthyfy-api.onrender.com/";
+  // static domain = "http://localhost:5000/";
+  static domain = "https://healthyfy-api.onrender.com/";
 }
 export const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
